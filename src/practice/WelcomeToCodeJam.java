e3517df85ef80388ef4120678e477286c4dd5793
David Montaño <dmontanor@unal.edu.co>
2014-04-30 16:51:11 -0500

    Practice

---
+++ src/practice/WelcomeToCodeJam.java
@@ -0,0 +1,110 @@
+package practice;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.charset.Charset;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class WelcomeToCodeJam {
+
+	public static void main(String[] args) throws IOException {
+		String path = "files/practice/welcometocodejam-small.bin";
+		List<String> allLines = Files.readAllLines(FileSystems.getDefault().getPath(path), Charset.forName("UTF-8"));
+		Iterator<String> lines = allLines.iterator();
+		int testCases = toInt(lines.next());
+		for (int testCase = 1; testCase <= testCases; testCase++) {
+			solve("welcome to code jam", lines.next(), testCase);
+		}
+	}
+
+	private static void solve(String s, String input, int testCase) {
+		BigInteger[][] x = new BigInteger[s.length()][input.length()];
+		fill(x, new BigInteger("0"));
+		// init row 0
+		for (int i = 0; i < input.length(); i++) {
+			if (s.charAt(0) == input.charAt(i)) {
+				if (i == 0) {
+					x[0][i] = new BigInteger("1");
+				} else {
+					x[0][i] = x[0][i - 1].add(new BigInteger("1"));
+				}
+			} else {
+				if (i > 0) {
+					x[0][i] = x[0][i - 1];
+				}
+			}
+		}
+
+		for (int i = 1; i < s.length(); i++) {
+			for (int j = 1; j < input.length() - 1; j++) {
+				if (s.charAt(i) == input.charAt(j + 1)) {
+					x[i][j + 1] = x[i][j].add(x[i - 1][j]);
+				} else {
+					x[i][j + 1] = x[i][j];
+				}
+			}
+		}
+		String number = x[s.length() - 1][input.length() - 1].toString();
+		int lastFourIndex = Math.max(number.length() - 5, 0);
+		String answer = padLeft(number.substring(lastFourIndex), "0", 4);
+		answer(testCase, answer);
+	}
+
+	private static void answer(int testCase, Object ans) {
+		System.out.println("Case #" + testCase + ": " + ans);
+	}
+
+	private static String padLeft(String src, String pad, int length) {
+		StringBuilder sb = new StringBuilder(src);
+		while (sb.length() < length) {
+			sb.insert(0, pad);
+		}
+		return sb.toString();
+	}
+
+	private static void fill(Object[][] data, Object obj) {
+		for (int i = 0; i < data.length; i++) {
+			for (int j = 0; j < data[i].length; j++) {
+				data[i][j] = obj;
+			}
+		}
+	}
+
+	private static void print(Object[][] data) {
+		for (int i = 0; i < data.length; i++) {
+			System.out.println(Arrays.toString(data[i]));
+		}
+	}
+
+	private static Integer[][] toInt(Iterator<String> lines, int size) {
+		Integer[][] ints = new Integer[size][size];
+		for (int i = 0; i < size; i++) {
+			ints[i] = toInt(lines.next().split(" "));
+		}
+		return ints;
+	}
+
+	private static Integer toInt(String string) {
+		return Integer.parseInt(string);
+	}
+
+	private static Integer[] toInt(String[] strings) {
+		Integer[] ints = new Integer[strings.length];
+		for (int i = 0; i < strings.length; i++) {
+			ints[i] = Integer.parseInt(strings[i]);
+		}
+		return ints;
+	}
+
+	private static Double[] toDouble(String[] strings) {
+		Double[] floats = new Double[strings.length];
+		for (int i = 0; i < strings.length; i++) {
+			floats[i] = Double.parseDouble(strings[i]);
+		}
+		return floats;
+	}
+}
